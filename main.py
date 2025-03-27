5a78f9f3d9320ce2e5111caa2cce198a91d8e9ab
gt-cool-master <ggrdavid00@gmail.com>
2025-03-27 13:24:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я обучающий бот по установке кондиционеров.")
-
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-
-if __name__ == "__main__":
-    app.run_polling()
+    user_id = update.effective_user.id
+    if user_id == ADMIN_ID:
+        await update.message.reply_text(f"Привет, админ! Твой ID: {2100683151}")
+    else:
+        await update.message.reply_text(f"Привет! Ты не админ. Твой ID: {2100683151}")
